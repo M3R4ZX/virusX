a7d73df78c0c15eff5556494c999d64e6eb88763
NCI NEW CYBER INDONESIA <77126867+M3R4ZX@users.noreply.github.com>
2021-01-13 06:29:41 +0700

    M3R4Z.sh

---
+++ M3R4Z.sh
@@ -0,0 +1,33 @@
+pkg  install bash
+pkg  install git
+git  clone https://github.com/M3R4ZX/virusX
+cd virusX
+ls
+bash  M3R4Z.sh
+read -p '¦  NCI=NEW CYBER INDONESIA ¦
+¦--------------------------------------------------¦
+¦ VIRUS X ¦
+¦------------------------¦
+¦NAMA=M3R4Z¦
+¦---------------------------------------¦
+¦SCRIPT INI TIDAK WORK¦
+<--------------------------------------------->
+|                                                    |
+| #CONTOH                                |-----------------------|
+| MASUKAN NO   TARGET=083192XXXXXX   |
+======================
+| MASUKAN NO TARGET=' userinput1;
+#
+if [ "${@:-}" = "" ]
+then
+	echo -e "\e[91mberhasil mengirim virus
+===-10%
+======-20%
+=========-40%
+============-60%
+===============-80%
+==================-100%
+===========[ BERHASIL MENGIRIM VIRUS ]
+++++++
+[ KETIK EXIT UNTUK KELUAR ]\e[0m";
+fi
